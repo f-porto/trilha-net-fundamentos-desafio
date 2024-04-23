caa74d70c4b9aa2da8c1628c7e2980fced0498e9
f-porto <felipe.portosa@gmail.com>
2024-04-23 18:51:26 -0300

    fix: user can press any key instead of enter

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -53,7 +53,8 @@ while (exibirMenu)
     }
 
     Console.WriteLine("Pressione uma tecla para continuar");
-    Console.ReadLine();
+    Console.ReadKey();
+    Console.WriteLine();
 }
 
 Console.WriteLine("O programa se encerrou");
